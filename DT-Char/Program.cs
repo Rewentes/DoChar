b9f77e3ae77628a3f0c58653a8483a44dbcda200
Rewen <evgeniy.movchan.03@gmail.com>
2022-04-02 23:42:24 +1000

    Added a white and blacklist, corrected the code and corrected the bugs

--- DT-Char/Program.cs
+++ DT-Char/Program.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 class Program
 {
-    static bool isRusChar(char Ch)
+  static bool isRusChar(char Ch)
     {
-        if ((int)Ch >= 1040 && (int)Ch <= 1105 || (int)Ch == 1025)
+        if ((int)Ch >= 1040 && (int)Ch <= 1105 || (int)Ch == 1025 || (int)Ch ==1105)
         {
           return true;
         }
@@ -35,37 +35,68 @@ class Program
     {
       if (((int)Ch >= 48 && (int)Ch <= 57) ||
             ((int)Ch >= 65 && (int)Ch <= 90 || (int)Ch >= 97 && (int)Ch <= 122) ||
-            ((int)Ch >= 1040 && (int)Ch <= 1105 || (int)Ch == 1025) || Ch == ' ')
+            ((int)Ch >= 1040 && (int)Ch <= 1105 || (int)Ch == 1025 || (int)Ch ==1105))
       {
         return true;
       }
     return false;
     }
 
-    static bool isBlackListChar(char Ch, HashSet<String> blackList)
+    static HashSet<char> addBlackList(string Str, HashSet<char> blackList)
     {
-      foreach (var item in blackList) 
+      foreach (var c in Str.Split("", ','))
       {
-        foreach (var chr in item) 
-        {
-          if (Ch == chr) return true;
-        }
+        blackList.Add(Convert.ToChar(c)); 
       }
-    return false;
+      return blackList;
+    }
+    static bool isBlackListChar(char Ch, HashSet<char> blackList)
+    {
+      foreach (var c in blackList) 
+      {
+        if (Ch == c) return false;
+      }
+      return true;
+    }
+    
+    static HashSet<char> addWhiteList(string Str, HashSet<char> whiteList)
+    {
+      foreach (var c in Str.Split("", ','))
+      {
+        whiteList.Add(Convert.ToChar(c)); 
+      }
+      return whiteList;
+    }
+    static bool isWhiteListChar(char Ch, HashSet<char> whiteList)
+    {
+      foreach (var c in whiteList) 
+      {
+        if (Ch == c) return true;
+      }
+      return false;
     }
 
     static char toLow(string low, char Ch)
     {
-      if ((low == "Да") && ((int)Ch >= 65 && (int)Ch <= 90) || ((int)Ch >= 1040 && (int)Ch <= 1071)) 
+      
+      if (low == "Да") 
       {
-        return (char) ((int) Ch + 32);
+        if (((int)Ch >= 65 && (int)Ch <= 90) || ((int)Ch >= 1040 && (int)Ch <= 1071)) 
+        {
+          return (char) ((int) Ch + 32);
+        }
+        if ((int)Ch == 1025)
+        {
+          return (char) ((int) Ch + 80);
+        }
       }
+      
       else if (low == "Нет") return Ch;
       
       return '0';
     }
 
-    static string Logic(string str, string symbol, string low) 
+    static string Logic(string str, string symbol, string low, HashSet<char> blackList, HashSet<char> whiteList) 
     {
       bool rus = true;
       bool eng = true;
@@ -85,23 +116,33 @@ class Program
         eng = false;
         num = true;     
       }
-      
+
       StringBuilder sb = new StringBuilder();
+      
       foreach(char c in str) {
         
-        if (rus && isRusChar(c)) {
+        if (isWhiteListChar(c, whiteList) || (rus && isRusChar(c) && isBlackListChar(c, blackList))) 
+        {
           sb.Append(toLow(low, c));
             continue;
         }
         
-        if (eng && isEngChar(c)) {
+        if (isWhiteListChar(c, whiteList) || (rus && isRusChar(c) && isBlackListChar(c, blackList))) 
+        {
           sb.Append(toLow(low, c));
             continue;
         }
         
-        if (num && isNumChar(c)) {
+        if (isWhiteListChar(c, whiteList) || (rus && isRusChar(c) && isBlackListChar(c, blackList))) 
+        {
           sb.Append(toLow(low, c));
             continue;
+        }
+
+        else if (isWhiteListChar(c, whiteList) || (isRusChar(c) && isBlackListChar(c, blackList))) 
+        {
+          sb.Append(toLow(low, c));
+          continue;
         }
       } 
       return sb.ToString();
@@ -109,27 +150,32 @@ class Program
 
     static void Main()
     {
-      HashSet<String> whiteList = new HashSet<String>();
-      HashSet<String> blackList = new HashSet<String>();
-      String symbol, low, black;
+      String str, symbol, low, black, white;
+      HashSet<char> blackList = new HashSet<char>();
+      HashSet<char> whiteList = new HashSet<char>();
     
       Console.WriteLine("Какие символы оставить?: (Русский, Английский, Цифры)");
       symbol = Console.ReadLine();
     
+      Console.WriteLine("Добавить символ\\слово(а) в whiteList?: Да\\Нет");
+      white = Console.ReadLine();
       Console.WriteLine("Добавить символ\\слово(а) в BlackList?: Да\\Нет");
       black = Console.ReadLine();
 
+      if (white == "Да") {
+        Console.WriteLine("Введите символ\\слово(а) в whiteList:");
+        whiteList = addBlackList(Console.ReadLine(), whiteList);
+      }
       if (black == "Да") {
-        Console.WriteLine("Введите символ\\слово(а):");
-        foreach(var elem in Console.ReadLine().Split(' ', ','))
-          blackList.Add(elem); 
+        Console.WriteLine("Введите символ\\слово(а) в BlackList:");
+        blackList = addBlackList(Console.ReadLine(), blackList);
       }
       
       Console.WriteLine("Перевести в нижний регистр?: Да\\Нет");
       low = Console.ReadLine();
 
       Console.WriteLine("Введите строку:");
-      String str = Console.ReadLine();
-      Console.WriteLine(Logic(str, symbol, low));
+      str = Console.ReadLine();
+      Console.WriteLine(Logic(str, symbol, low, blackList, whiteList));
     }
 }
