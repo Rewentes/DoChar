51844d00f8bdaf2f62c3fcdf6c91acc26ab6e96d
Rewen <evgeniy.movchan.03@gmail.com>
2022-04-03 22:38:01 +1000

    Unit tests 0.2

---
+++ DoChar.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DT_Char.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DT_Char.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8C0D1817-7DE2-4BC9-9562-DB9F7D56C322")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DoChar.Tests/Tests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DoChar.Tests
+{
+    [TestFixture]
+    public class DoCharTests
+    {
+        [Test]
+        public void isRusChar_RusChar_TrueReturned()
+        {
+            String str0 = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ";
+            String str1 = "йцукенгшщзхъфывапролджэячсмитьбюё";
+            bool exp = true;
+            DoChar dt = new DoChar();
+            foreach (var ch in str0)
+            {
+                bool act = dt.isRusChar(ch);
+                Assert.AreEqual(exp, act);
+            }
+
+            foreach (var ch in str1)
+            {
+                bool act = dt.isRusChar(ch);
+                Assert.AreEqual(exp, act);
+            }
+
+        }
+
+        public void isEngChar_EngChar_TrueReturned()
+        {
+            String str0 = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            String str1 = "qwertyuiopasdfghjklzxcvbnm";
+            bool exp = true;
+            DoChar dt = new DoChar();
+            foreach (var ch in str0)
+            {
+                bool act = dt.isEngChar(ch);
+                Assert.AreEqual(exp, act);
+            }
+
+            foreach (var ch in str1)
+            {
+                bool act = dt.isEngChar(ch);
+                Assert.AreEqual(exp, act);
+            }
+        }
+
+        public void isNumChar_NumChar_TrueReturned()
+        {
+            String str = "1234567890";
+            bool exp = true;
+            DoChar dt = new DoChar();
+            foreach (var ch in str0)
+            {
+                boll act = dt. isNumChar(ch);
+                Assert.AreEqual(exp, act);
+            }
+        }
+        
+        public void toLow_bCharEng_sCharEngReturned()
+        {
+            String str0 = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            String exp = "qwertyuiopasdfghjklzxcvbnm";
+            DoChar dt = new DoChar();
+            foreach (var ch in str0)
+            {
+                String act = dt.toLow("Да", ch);
+                Assert.AreEqual(exp, act);
+            }
+        }
+        
+        public void toLow_bCharRus_sCharRusReturned()
+        {
+            String str0 = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ";
+            String exp = "йцукенгшщзхъфывапролджэячсмитьбюё";
+            DoChar dt = new DoChar();
+            foreach (var ch in str0)
+            {
+                String act = dt.toLow("Да", ch);
+                Assert.AreEqual(exp, act);
+            }
+        }
+        
+        public void isBlackListChar_Ch_FalseReturned()
+        {
+            HashSet<char> blackList = new HashSet<char>(); blackList.Add('0');
+            bool exp = false;
+            DoChar dt = new DoChar();
+            String act = dt.isBlackListChar('0', blackList);
+            Assert.AreEqual(exp, act);
+        }
+        
+        public void isWhiteListChar_Ch_TrueReturned()
+        {
+            HashSet<char> blackList = new HashSet<char>(); blackList.Add('0');
+            bool exp = true;
+            DoChar dt = new DoChar();
+            String act = dt.isBlackListChar('0', blackList);
+            Assert.AreEqual(exp, act);
+        }
+        
+        public void Logic_string_clearStringReturned()
+        {
+            HashSet<char> blackList = new HashSet<char>();  
+            HashSet<char> whiteList = new HashSet<char>();
+            whiteList.Add('@');
+            blackList.Add('0');
+            String str = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ QWERTYUIOPASDFGHJKLZXCVBNM 1234567890 ~!@#$%^&*()_+}|{:>?}";
+            String exp = "йцукенгшщзхъфывапролджэячсмитьбюё qwertyuiopasdfghjklzxcvbnm 123456789 @";
+            DoChar dt = new DoChar();
+            String act = dt.Logic(str, "", "Да", blackList, whiteList);
+            Assert.AreEqual(exp, act);
+            
+        }
+    }
+}
